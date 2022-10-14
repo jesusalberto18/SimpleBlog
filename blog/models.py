b8332f95db6d22c91d124610c20fbc77812cff80
Alberto <albertiae.digital@gmail.com>
2022-10-14 13:52:59 -0400

    Defined the shape of the body of posts in the model class Posts()

--- blog/models.py
+++ blog/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
+    intro = models.TextField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
